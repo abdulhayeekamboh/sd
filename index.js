09da524adee093f2563931d78ce8f1a048a582af
Abdul HAYEE KAMBOH <159583911+abdulhayeekamboh@users.noreply.github.com>
2026-02-16 15:12:41 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import fetch from "node-fetch";
 import dotenv from "dotenv";
+import cors from 'cors';
 
 dotenv.config();
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.post("/secure-download", async (req, res) => {
